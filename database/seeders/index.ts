4ddc86057b6e20080a004007c65feb59ca0068d4
erick <erickluiz69@gmail.com>
2022-07-05 22:28:32 -0300

    chore: change factories and seed

--- database/seeders/index.ts
+++ database/seeders/index.ts
@@ -1,12 +1,37 @@
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
-import { AuthorFactory } from 'Database/factories'
+import QuoteTag from 'App/Models/QuoteTag'
+import Tag from 'App/Models/Tag'
+import { AuthorFactory, QuoteFactory } from 'Database/factories'
 
-export default class AuthorSeeder extends BaseSeeder {
+const TAGS = [
+  'funny',
+  'sad',
+  'wisdom',
+  'inspirational',
+  'educational',
+  'life',
+  'love',
+  'motivational',
+]
+
+export default class Seeder extends BaseSeeder {
   public async run() {
-    await AuthorFactory.with('quotes', 10, (quoteFactory) => {
-      quoteFactory.with('tags', 3, (tagFactory) => {
-        tagFactory.with('tag', 2)
-      })
-    }).createMany(10)
+    const tags = await Tag.createMany(TAGS.map((name) => ({ name, slug: name })))
+
+    const authors = await AuthorFactory.createMany(20)
+
+    const quotes = await QuoteFactory.createMany(100, (model) => {
+      const author = authors[Math.floor(Math.random() * authors.length)] ?? authors[0]
+
+      model.authorId = author.id
+    })
+
+    const quoteTags = quotes.map((quote) => {
+      const tag = tags[Math.floor(Math.random() * tags.length)] ?? tags[0]
+
+      return { quoteId: quote.id, tagId: tag.id }
+    })
+
+    await QuoteTag.createMany(quoteTags)
   }
 }
